07c55de2da4745ca0687678552bc86296d1c42a9
candykise <459295866@qq.com>
2020-07-10 00:28:28 +0800

    小练习

---
+++ demo01.py
@@ -0,0 +1,19 @@
+# a=float(input("请输入："))
+# b=float(input("请输入："))
+# print("input获取的内容:",a+b)
+
+# print(type(2333))
+# print(type("哈哈"))
+# print(type(2.333))
+# print(type(True))
+# print(type([]))
+# print(type({}))
+
+# a="kkjhsfjhdfskjshfdkjshfkjshfjk"
+# print(len(a))
+a=input("请输入：")
+b=input("请输入：")
+print(len(a+b))
+
+input("请输入")
+77
