4aa4918d6499537d650cc5ba07d8ea26fbd48a01
candykise <459295866@qq.com>
2020-07-10 00:27:44 +0800

    判断

---
+++ demo02.py
@@ -0,0 +1,48 @@
+# a = 32
+# b = 2
+#判断 if
+# if a == 1:
+#     print("a是自然数")
+# #if else 
+# if a > b:
+#     print("a比b大")
+# else:
+#     print("b更大")
+#if elif else
+# a = int(input("请输入你的年龄："))
+# if a > 20 and a <= 30:
+#     print("你还年轻")
+# elif a > 30 and a <= 40:
+#     print("你该结婚生子了")
+# elif a > 40 and a <=50:
+#     print("你已经不年轻了")
+# else:
+#     print("你该退休了")
+a = input("请输入：")
+# if a == "":
+#     print("请输入数字")
+# if a in "0123456789":
+#     a = int(a)
+# else:
+#     print("请输入正确的数字")
+#     exit()
+# if a > 5:
+#     print("大")
+# else:
+#     print("小")
+int(a)
+if a in ["你好，今天你吃了吗",1,2,3,4,5]:
+    print("a是字符串")
+elif a in "013456789":
+    print("a是数字")
+else:
+    print("a是其他")
+
+# a = "你好"
+# if type(a) is int:
+#     print("是数字")
+# elif type(a) is str:
+#     print("是字符串")
+# else:
+#     print("是其他")
+
