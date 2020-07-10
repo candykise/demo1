cf6ed62cedce9180e9e66f9b413ce378a2b9212b
candykise <459295866@qq.com>
2020-07-10 22:38:26 +0800

    练习

---
+++ zhangsan.py
@@ -0,0 +1,41 @@
+# # print("hello world!")
+# # # len("hjskfhjkdshfkjdshf")
+print(len("jkshdsjfhkjdshf"))
+# # print(type(2333))
+# # a = (1,2,3,4,"哈哈","哈哈","嘻嘻",True,False)
+# # print(a[2])
+# # print(a.index("嘻嘻"))
+# # print(a.count("哈哈"))
+# # # print(a[0:3])
+# # b=[1,2,3,4,"呵呵","啦啦","啦啦",True,False]
+# # # print(b.index("呵呵"))
+# # # print(b.count("啦啦"))
+# # # b.append("你好")
+# # # b.insert(2,233)
+# # # b.pop(3)
+
+
+# # # print(b)
+
+# # a=b.pop(0)
+# # c=b.pop(1)
+# # print(a+c)
+# # e = [1,2,3]
+# # b.extend(e)
+# # b.remove("呵呵")
+# # print(b)
+a = {"name":"candy","age":"16","high":"168cm"}
+# # print(a["name"])
+# # a.pop("age")
+# # a["sex"]="男"
+# # a["name"]="lily"
+# # print(a.get("name"))
+a.update(adress=1)
+# 
+# # del a["name"]
+print(a)
+
+# a = input("请输入你的姓名：")
+# b = input("请输入你的年龄：")
+# c = input("请输入你的性别：")
+# print({"name":a,"age":b,"sex":c})
